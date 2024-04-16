cd2b226d7c7bc4a6260438d716ee8835fd8e1c92
bunnmei <kombinat.web@gmail.com>
2024-04-16 12:08:56 +0900

    1-9数字表示

--- main.c
+++ main.c
@@ -7,44 +7,57 @@
 #define SRCLK     (19) //PIN_CLK 
 #define RCLK     (20) //PIN_LATCH
 
-const uint8_t PATTERNS[] = {
-  0b00000001,
-  0b00000010,
-  0b00000100,
-  0b00001000,
-  0b00010000,
-  0b00100000,
-  0b01000000,
-  0b10000000,
+uint8_t patterns[] = {
+  0b01100000, //1
+  0b11011010,
+  0b11110010,
+  0b01100110,
+  0b10110110, //5
+  0b10111110,
+  0b11100000,
+  0b11111110,
+  0b11110110,
 };
 
 void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t val){
 
   uint8_t i;
+
   for (i = 0; i < 8; i++)  {
     gpio_put(dataPin, val & 1);
     val >>= 1;
+    gpio_put(clockPin, 1);
+    gpio_put(clockPin, 0);
   }
-
-  gpio_put(clockPin, 1);
-  gpio_put(clockPin, 0);
 }
 
 int main()
 {
     stdio_init_all();
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_put(LED_PIN, 1);
 
-    int max_pattern = sizeof(PATTERNS)/sizeof(PATTERNS[0]);
+    gpio_init(SER);
+    gpio_init(RCLK);
+    gpio_init(SRCLK);
+    gpio_set_dir(SER, GPIO_OUT);
+    gpio_set_dir(RCLK, GPIO_OUT);
+    gpio_set_dir(SRCLK, GPIO_OUT);
 
     while (1) {
+
+
     
-        for(uint8_t i=0; i < max_pattern; i++){
-          shiftOut(SER, SRCLK, PATTERNS[i]);
+        for(uint8_t i=0; i < sizeof(patterns)/sizeof(uint8_t); i++){
 
           gpio_put(RCLK, 0);
+          shiftOut(SER, SRCLK, patterns[i]);
           gpio_put(RCLK, 1);
 
-          sleep_ms(200);
+          sleep_ms(1000);
+     
         }
 
     }
